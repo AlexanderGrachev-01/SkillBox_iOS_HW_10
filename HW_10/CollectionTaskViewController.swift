ccfa203d3f8dd2f49d02c62ffc130779aa183594
Alexander Grachev <s89519011368@yandex.ru>
2021-02-02 16:20:05 +0300

    Initial commit

--- HW_10/CollectionTaskViewController.swift
+++ HW_10/CollectionTaskViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CollectionTaskViewController.swift
 //  HW_10
 //
 //  Created by Александр Грачев on 02.02.2021.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CollectionTaskViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
