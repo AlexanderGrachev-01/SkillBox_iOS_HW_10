667b4b606852ea2a7fb609f1fd2e478ed76bfded
Alexander <62618072+AlexanderGrachev-01@users.noreply.github.com>
2021-03-04 21:02:29 +0300

    add collection and png

--- HW_10/CollectionTaskViewController.swift
+++ HW_10/CollectionTaskViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class CollectionTaskViewController: UIViewController {
+    
+    var imageList = ["sneakers1.png", "sneakers2.png", "sneakers3.png", "sneakers4.png", "sneakers5.png", "sneakers6.png"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
+    
+//    extension CollectionTaskViewController: UICollectionViewDataSource {
+//        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//            return imageList.count
+//        }
+//    }
+    
 }
---
+++ HW_10/SneakersCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SneakersCollectionViewCell.swift
+//  HW_10
+//
+//  Created by Александр Грачев on 04.03.2021.
+//
+
+import UIKit
+
+class SneakersCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sneakersImageView: UIImageView!
+    
+    @IBOutlet weak var sneakersNameLabel: UILabel!
+    
+    @IBOutlet weak var sneakersPriceLabel: UILabel!
+    
+}
